0c13deba42da8655419b852486667648eb1bd2ff
mrf7777 <micryfulg@gmail.com>
2023-03-13 16:54:35 -0500

    start implementing beat calculations

--- src/rhythm.rs
+++ src/rhythm.rs
@@ -1,4 +1,9 @@
+pub trait ToRatio {
+    fn to_ratio(&self) -> f64;
+}
+
 #[derive(Clone, PartialEq, Eq, Debug)]
+
 pub struct TimeSignature {
     numerator: u16,
     denominator: u16,
@@ -25,6 +30,12 @@ impl TimeSignature {
     }
 }
 
+impl ToRatio for TimeSignature {
+    fn to_ratio(&self) -> f64 {
+        self.numerator as f64 / self.denominator as f64
+    }
+}
+
 #[derive(Clone, PartialEq, Debug)]
 pub struct Tempo {
     tempo_bpm: f64,
@@ -39,12 +50,16 @@ impl Tempo {
         Some(Self { tempo_bpm })
     }
 
-    pub fn tempo_bpm(&self) -> f64 {
+    pub fn bpm(&self) -> f64 {
         self.tempo_bpm
     }
 
-    pub fn tempo_bps(&self) -> f64 {
+    pub fn bps(&self) -> f64 {
         self.tempo_bpm / 60.0
+    }
+
+    pub fn one_beat_seconds(&self) -> f64 {
+        1.0 / self.bps()
     }
 }
 
@@ -60,6 +75,18 @@ impl BeatAssignment {
 
     pub fn beat_duration(&self) -> &Duration {
         &self.duration
+    }
+
+    pub fn beats_in_duration(&self, duration: &Duration) -> f64 {
+        let beat_assignment_ratio = self.beat_duration().to_ratio();
+        let duration_ratio = duration.to_ratio();
+        duration_ratio / beat_assignment_ratio
+    }
+}
+
+impl ToRatio for BeatAssignment {
+    fn to_ratio(&self) -> f64 {
+        self.duration.to_ratio()
     }
 }
 
@@ -84,6 +111,12 @@ impl Duration {
     }
 }
 
+impl ToRatio for Duration {
+    fn to_ratio(&self) -> f64 {
+        self.signature.to_ratio()
+    }
+}
+
 #[derive(Clone, PartialEq, Debug)]
 pub struct Rhythm {
     tempo: Tempo,
@@ -104,6 +137,11 @@ impl Rhythm {
 
     pub fn beat_assignment(&self) -> &BeatAssignment {
         &self.beat_assignment
+    }
+
+    pub fn seconds_from_duration(&self, duration: &Duration) -> f64 {
+        let beats_in_duration = self.beat_assignment().beats_in_duration(duration); // TODO:
+        beats_in_duration * self.tempo.one_beat_seconds()
     }
 }
 
@@ -137,7 +175,7 @@ mod tests {
     #[test]
     fn tempo_bpm_bps() {
         let tempo1 = Tempo::new(120.0).unwrap();
-        assert!((tempo1.tempo_bpm() - 120.00).abs() < 0.05);
-        assert!((tempo1.tempo_bps() - 2.00).abs() < 0.05);
+        assert!((tempo1.bpm() - 120.00).abs() < 0.05);
+        assert!((tempo1.bps() - 2.00).abs() < 0.05);
     }
 }

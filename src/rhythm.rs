1a29ae883b9e85ebb2801dba00e3dd1ca7298bb2
mrf7777 <micryfulg@gmail.com>
2023-03-13 13:16:55 -0500

    basic implementation of rhythm

--- src/rhythm.rs
+++ src/rhythm.rs
@@ -1,11 +1,11 @@
 #[derive(Clone, PartialEq, Eq, Debug)]
-struct TimeSignature {
-    numerator: u8,
-    denominator: u8,
+pub struct TimeSignature {
+    numerator: u16,
+    denominator: u16,
 }
 
 impl TimeSignature {
-    pub fn new(numerator: u8, denominator: u8) -> Option<TimeSignature> {
+    pub fn new(numerator: u16, denominator: u16) -> Option<Self> {
         if numerator == 0 || denominator == 0 {
             return None;
         }
@@ -14,5 +14,118 @@ impl TimeSignature {
             numerator,
             denominator,
         })
+    }
+
+    pub fn numerator(&self) -> u16 {
+        self.numerator
+    }
+
+    pub fn denominator(&self) -> u16 {
+        self.denominator
+    }
+}
+
+#[derive(Clone, PartialEq, Debug)]
+pub struct Tempo {
+    tempo_bpm: f64,
+}
+
+impl Tempo {
+    pub fn new(tempo_bpm: f64) -> Option<Self> {
+        if tempo_bpm < 0.0 {
+            return None;
+        }
+
+        Some(Self { tempo_bpm })
+    }
+
+    pub fn tempo_bpm(&self) -> f64 {
+        self.tempo_bpm
+    }
+
+    pub fn tempo_bps(&self) -> f64 {
+        self.tempo_bpm / 60.0
+    }
+}
+
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub struct BeatAssignment {
+    duration: Duration,
+}
+
+impl BeatAssignment {
+    pub fn new(duration: Duration) -> Self {
+        Self { duration }
+    }
+
+    pub fn beat_duration(&self) -> &Duration {
+        &self.duration
+    }
+}
+
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub struct Duration {
+    signature: TimeSignature,
+}
+
+impl Duration {
+    pub fn new(numerator: u16, denominator: u16) -> Option<Self> {
+        Some(Self {
+            signature: TimeSignature::new(numerator, denominator)?,
+        })
+    }
+
+    pub fn numerator(&self) -> u16 {
+        self.signature.numerator()
+    }
+
+    pub fn denominator(&self) -> u16 {
+        self.signature.denominator()
+    }
+}
+
+#[derive(Clone, PartialEq, Debug)]
+pub struct Beat {
+    tempo: Tempo,
+    beat_assignment: BeatAssignment,
+}
+
+impl Beat {
+    pub fn new(tempo: Tempo, beat_assignment: BeatAssignment) -> Self {
+        Self {
+            tempo,
+            beat_assignment,
+        }
+    }
+
+    pub fn tempo(&self) -> &Tempo {
+        &self.tempo
+    }
+
+    pub fn beat_assignment(&self) -> &BeatAssignment {
+        &self.beat_assignment
+    }
+}
+
+#[derive(Clone, PartialEq, Debug)]
+pub struct Metre {
+    beat: Beat,
+    time_signature: TimeSignature,
+}
+
+impl Metre {
+    pub fn new(beat: Beat, time_signature: TimeSignature) -> Self {
+        Self {
+            beat,
+            time_signature,
+        }
+    }
+
+    pub fn beat(&self) -> &Beat {
+        &self.beat
+    }
+
+    pub fn time_signature(&self) -> &TimeSignature {
+        &self.time_signature
     }
 }

843639f574b006f0ffa0ed93e223ea1d853ec971
mrf7777 <micryfulg@gmail.com>
2023-03-15 12:20:15 -0500

    implement basic functions for harmony structs

--- src/harmony.rs
+++ src/harmony.rs
@@ -1,10 +1,22 @@
+#![deny(clippy::all, clippy::pedantic)]
+
 use std::collections::HashSet;
 
 use crate::pitch;
 
 #[derive(Clone, PartialEq, Eq, Debug)]
 pub struct ChordClass {
-    notes: HashSet<pitch::NotePitchClass>,
+    note_pitch_classes: HashSet<pitch::NotePitchClass>,
+}
+
+impl ChordClass {
+    pub fn new(note_pitch_classes: HashSet<pitch::NotePitchClass>) -> Self {
+        Self { note_pitch_classes }
+    }
+
+    pub fn note_pitch_classes(&self) -> &HashSet<pitch::NotePitchClass> {
+        &self.note_pitch_classes
+    }
 }
 
 #[derive(Clone, PartialEq, Eq, Debug)]
@@ -13,13 +25,51 @@ pub struct RootedChordClass {
     root: pitch::NotePitchClass,
 }
 
+impl RootedChordClass {
+    pub fn new(chord_class: ChordClass, root: pitch::NotePitchClass) -> Self {
+        Self { chord_class, root }
+    }
+
+    pub fn chord_class(&self) -> &ChordClass {
+        &self.chord_class
+    }
+
+    pub fn root(&self) -> pitch::NotePitchClass {
+        self.root
+    }
+}
+
 #[derive(Clone, PartialEq, Eq, Debug)]
 pub struct Chord {
-    notes: HashSet<pitch::NotePitch>,
+    note_pitches: HashSet<pitch::NotePitch>,
+}
+
+impl Chord {
+    pub fn new(note_pitches: HashSet<pitch::NotePitch>) -> Self {
+        Self { note_pitches }
+    }
+
+    pub fn note_pitchs(&self) -> &HashSet<pitch::NotePitch> {
+        &self.note_pitches
+    }
 }
 
 #[derive(Clone, PartialEq, Eq, Debug)]
 pub struct RootedChord {
     chord: Chord,
     root: pitch::NotePitch,
+}
+
+impl RootedChord {
+    pub fn new(chord: Chord, root: pitch::NotePitch) -> Self {
+        Self { chord, root }
+    }
+
+    pub fn chord(&self) -> &Chord {
+        &self.chord
+    }
+
+    pub fn root(&self) -> pitch::NotePitch {
+        self.root
+    }
 }

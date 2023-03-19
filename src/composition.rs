1bdc21ef2776e758a6e040ffff6265a6f6ace528
mrf7777 <micryfulg@gmail.com>
2023-03-19 11:09:54 -0500

    remove unneeded structs from composition

--- src/composition.rs
+++ src/composition.rs
@@ -5,18 +5,6 @@ use std::collections::BTreeMap;
 use crate::rhythm;
 
 #[derive(Clone, PartialEq, Eq, Debug)]
-pub struct RelativeTimeline<V> {
-    elements: BTreeMap<rhythm::Duration, V>,
-}
-
-#[derive(Clone, PartialEq, Debug)]
 pub struct Timeline<V> {
-    relative_timeline: RelativeTimeline<V>,
-    tempo: rhythm::Tempo,
-}
-
-#[derive(Clone, PartialEq, Eq, Debug)]
-pub struct Marker {
-    name: String,
-    position: rhythm::Duration,
+    elements: BTreeMap<rhythm::Duration, V>,
 }
